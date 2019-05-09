d7c879bab8f591f5b863074e80fe9f42ab5ccf92
Kevin <kvng88@gmail.com>
2019-05-09 15:18:55 -0700

    added campaign slug to be displayed on admin

--- src/Control.js
+++ src/Control.js
@@ -2,6 +2,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class Control extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: ''
+    }
+  }
   static propTypes = {
     onChange: PropTypes.func.isRequired,
     forID: PropTypes.string,
@@ -13,22 +19,32 @@ export default class Control extends React.Component {
     value: '',
   }
 
+  onChangeHandler = (e) => {
+    this.setState({url: e.target.value})
+  }
+
   render() {
     const {
       forID,
-      value,
-      onChange,
       classNameWrapper,
     } = this.props;
-
+    const styles = {
+      marginBottom: 0,
+      fontSize: '12px',
+      color: 'rgb(122, 130, 145)',
+      padding: '3px 0px'
+    }
     return (
+      <div>
       <input
         type="text"
         id={forID}
         className={classNameWrapper}
-        value={value || ''}
-        onChange={e => onChange(e.target.value)}
+        value={this.state.url}
+        onChange={this.onChangeHandler}
       />
+      <div style={styles}>https://greetings.hollywoodrecords.com/{this.state.url || '<CAMPAIGN NAME>'}</div>
+      </div>
     );
   }
 }
